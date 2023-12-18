1d4ad21d4feadaa1ede21f61317c0b3188476b9d
Ethan Zhang <yzhang.wa@gmail.com>
2023-12-18 14:25:26 +0800

    to associate user address to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,7 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
+    @post.user_id = session[:user_id]
 
     respond_to do |format|
       if @post.save
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+  belongs_to :user, class_name: "RubyAuthMetamask::User", foreign_key: "user_id"
 end
